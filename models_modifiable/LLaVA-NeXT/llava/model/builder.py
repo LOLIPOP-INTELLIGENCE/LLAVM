654a55c0e24df240a51be048cedc330b587bb403
LOLIPOP-INTELLIGENCE <manaslegodesigns@gmail.com>
2024-11-08 11:41:24 +0800

    adds explainer comments for builder.py

--- models_modifiable/LLaVA-NeXT/llava/model/builder.py
+++ models_modifiable/LLaVA-NeXT/llava/model/builder.py
@@ -13,6 +13,8 @@
 #    limitations under the License.
 
 
+# Setup code for the llava model to load the correct model architecture, set up multimodal components, configuring tokenizers, manage model quantization
+
 import os
 import warnings
 import shutil
@@ -23,14 +25,16 @@ from llava.model import *
 from llava.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 from llava.utils import rank0_print
 
-
+# init parameters: paths to model, quantisation options (4 and 8 bit), etc
 def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="auto", torch_dtype="float16",attn_implementation="flash_attention_2", customized_config=None, overwrite_config=None, **kwargs):
     kwargs["device_map"] = device_map
 
+    # setup model loading params for quantisation preference
     if load_8bit:
         kwargs["load_in_8bit"] = True
     elif load_4bit:
         kwargs["load_in_4bit"] = True
+        # BitsAndBytes used as a quantisation technique
         kwargs["quantization_config"] = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4")
     elif torch_dtype == "float16":
         kwargs["torch_dtype"] = torch.float16
@@ -49,16 +53,19 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
     else:
         is_multimodal = False
 
+    # check if model is multimodal
     if "llava" in model_name.lower() or is_multimodal:
         # Load LLaVA model
         if "lora" in model_name.lower() and model_base is None:
             warnings.warn(
                 "There is `lora` in model name but no `model_base` is provided. If you are loading a LoRA model, please provide the `model_base` argument. Detailed instruction: https://github.com/haotian-liu/LLaVA#launch-a-model-worker-lora-weights-unmerged."
             )
+        # handle lora config
         if "lora" in model_name.lower() and model_base is not None:
             lora_cfg_pretrained = AutoConfig.from_pretrained(model_path)
             tokenizer = AutoTokenizer.from_pretrained(model_base, use_fast=False)
             rank0_print("Loading LLaVA from base model...")
+            # each model (mistral, mixtral, gemma, llama) has its own loading config
             if "mixtral" in model_name.lower():
                 from llava.model.language_model.llava_mixtral import LlavaMixtralConfig
 
@@ -113,6 +120,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             rank0_print("Merging LoRA weights...")
             model = model.merge_and_unload()
             rank0_print("Model is loaded...")
+        # no lora present in model config
         elif model_base is not None:  # this may be mm projector only, loading projector with preset language mdoel
             rank0_print(f"Loading LLaVA from base model {model_base}...")
             if "mixtral" in model_name.lower():
@@ -251,6 +259,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
                 except:
                     raise ValueError(f"Model {model_name} not supported")
 
+    # model is not multimodal
     else:
         # Load language model
         if model_base is not None:
@@ -277,15 +286,18 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
     rank0_print(f"Model Class: {model.__class__.__name__}")
     image_processor = None
 
+    # model is multimodal
     if "llava" in model_name.lower() or is_multimodal:
         mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
         mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
+        # special tokens to identify image tokens in the model's inputs
         if mm_use_im_patch_token:
             tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
         if mm_use_im_start_end:
             tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
         model.resize_token_embeddings(len(tokenizer))
 
+        # load vision tower for processing vision capabilities
         vision_tower = model.get_vision_tower()
         if not vision_tower.is_loaded:
             vision_tower.load_model(device_map=device_map)
@@ -293,6 +305,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             vision_tower.to(device="cuda", dtype=torch.float16)
         image_processor = vision_tower.image_processor
 
+    # load context length for model else set it to 2048 tokens
     if hasattr(model.config, "max_sequence_length"):
         context_len = model.config.max_sequence_length
     elif hasattr(model.config, "max_position_embeddings"):
