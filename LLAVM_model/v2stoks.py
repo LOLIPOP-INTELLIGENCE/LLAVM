dedaf73cd95685f63b4701b409d460b3db8d6ad1
DESU-CLUB <warrenlowkh@gmail.com>
2024-12-04 13:53:02 +0000

    Added semantic token converter

---
+++ LLAVM_model/v2stoks.py
@@ -0,0 +1,15 @@
+from whisperspeech.t2s_up_wds_mlang_enclm import TSARTransfromer
+
+class TTSenmanticToken:
+    def __init__(self, device: str) -> None:
+        self.t2s_model = TSARTransformer.load_model("collabora/whisperspeech:t2s-v1.9-medium-7lang.model", device = device)
+        self.t2s_model.optimize(torch_compile=True)
+    def convert_text_to_semantic(self,text: str):
+        """
+            Convert texts to semantic tokens
+        Args:
+            text (str): The text to convert to audio
+        Returns:
+            torch.Tensor: The generated audio
+        """
+        return self.t2s_model.generate(text, lang="en",cps=15, T =0.0, steps=None)
